83727dc7680af99b9b9d504a09d44735516db5d4
anhkhoa2174 <tkhoa4040@gmail.com>
2024-12-10 19:41:50 +0700

    Adjusted dependent

--- btl2/src/components/Dependent.jsx
+++ btl2/src/components/Dependent.jsx
@@ -21,7 +21,7 @@ function Dependent() {
 		window.history.back();
 	};
 
-	const [activeSection, setActiveSection] = useState("patientInfo");
+	const [activeSection, setActiveSection] = useState("myInfo");
 
 	const costData = {
 		medication: 1000000,
